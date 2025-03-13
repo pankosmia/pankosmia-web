9fb5ef827aa44d133096a522a7bf1fd86e0733b0
Mark Howe <mvahowe@gmail.com>
2025-03-13 18:44:39 +0100

    Rework SSE

--- src/endpoints/sse.rs
+++ src/endpoints/sse.rs
@@ -19,9 +19,13 @@ pub async fn notifications_stream<'a>(
 ) -> stream::EventStream![stream::Event + 'a] {
     stream::EventStream! {
         let mut count = 0;
-        let mut interval = time::interval(Duration::from_millis(500));
+        let mut interval = time::interval(Duration::from_millis(1000));
         yield stream::Event::retry(Duration::from_secs(1));
         let mut languages = state.languages.lock().unwrap().clone().join("/");
+        let mut typography = state.typography.lock().unwrap().clone();
+        let mut bcv = state.bcv.lock().unwrap().clone();
+        let gitea_endpoints = state.gitea_endpoints.clone();
+        let mut auth_tokens = state.auth_tokens.lock().unwrap().clone();
         loop {
             while !msgs.lock().unwrap().is_empty() {
                 let msg = msgs.lock().unwrap().pop_front().unwrap();
@@ -49,48 +53,41 @@ pub async fn notifications_stream<'a>(
             .event("debug")
             .id(format!("{}", count));
             count+=1;
-            if count % 4 == 0 {
-                let bcv = state.bcv.lock().unwrap().clone();
+            let new_bcv = state.bcv.lock().unwrap().clone();
+            if bcv.book_code != new_bcv.book_code || bcv.chapter != new_bcv.chapter || bcv.verse != new_bcv.verse || count < 10  {
+                bcv = new_bcv;
                 yield stream::Event::data(
                     format!("{}--{}--{}", bcv.book_code, bcv.chapter, bcv.verse)
                 )
                 .event("bcv")
                 .id(format!("{}", count));
                 count+=1;
-                /*
-            } else if count % 4 == 1 {
-                let typography = state.typography.lock().unwrap().clone();
+            }
+            let new_typography = state.typography.lock().unwrap().clone();
+                if typography.font_set != new_typography.font_set || typography.size != new_typography.size || typography.direction != new_typography.direction || count < 10  {
+                typography = new_typography;
                 yield stream::Event::data(
                     format!("{}--{}--{}", typography.font_set, typography.size, typography.direction)
                 )
                 .event("typography")
                 .id(format!("{}", count));
                 count+=1;
-                 */
-            } else if count % 4 == 2 {
-                let gitea_endpoints = state.gitea_endpoints.clone();
-                let auth_tokens = state.auth_tokens.lock().unwrap().clone();
-                for (ep_name, ep_endpoint) in gitea_endpoints {
+            }
+            let new_auth_tokens = state.auth_tokens.lock().unwrap().clone();
+            for (ep_name, ep_endpoint) in &gitea_endpoints {
+                let ep_name2 = ep_name.clone();
+                if new_auth_tokens.contains_key(&ep_name2) != auth_tokens.contains_key(&ep_name2) {
+                    auth_tokens.insert(ep_name.clone(), new_auth_tokens[&ep_name2].clone());
                     yield stream::Event::data(
-                        format!("{}--{}--{}", ep_name, ep_endpoint, auth_tokens.contains_key(&ep_name))
+                        format!("{}--{}--{}", ep_name, ep_endpoint, auth_tokens.contains_key(&ep_name2))
                     )
                     .event("auth")
                     .id(format!("{}", count));
                     count+=1;
                 }
-                /*
-            } else if count % 4 == 3 {
-                let languages = state.languages.lock().unwrap().clone();
-                yield stream::Event::data(
-                    format!("{}", languages.join("/"))
-                )
-                .event("languages")
-                .id(format!("{}", count));
-                count+=1;
-                 */
             }
             let new_languages = state.languages.lock().unwrap().clone().join("/");
-            if new_languages.clone() != languages.clone() {
+            if new_languages.clone() != languages.clone() || count < 10 {
                 languages = new_languages;
                 yield stream::Event::data(
                     format!("{}", languages.clone())
