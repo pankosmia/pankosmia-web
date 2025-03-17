0185fbdd23460f2673f4d2d0bc7af436260ad689
Mark Howe <mvahowe@gmail.com>
2025-03-17 15:44:49 +0100

    Do not spam with auth

--- src/endpoints/sse.rs
+++ src/endpoints/sse.rs
@@ -80,7 +80,7 @@ pub async fn notifications_stream<'a>(
                     .event("auth")
                     .id(format!("{}", count));
                     count+=1;
-                } else {
+                } else if count < 10 {
                     yield stream::Event::data(
                         format!("{}--{}--{}", ep_name, ep_endpoint, auth_tokens.contains_key(&ep_name2))
                     )
