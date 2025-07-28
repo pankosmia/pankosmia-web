843a1e012482c23013a5eb91bf71d7e646d16093
Mark Howe <mvahowe@gmail.com>
2025-07-28 13:36:03 +0200

    Copy/move ingredient endpoint

--- src/endpoints/burrito2/mod.rs
+++ src/endpoints/burrito2/mod.rs
@@ -11,3 +11,5 @@ pub mod post_bytes_ingredient;
 pub mod post_delete_ingredient;
 pub mod get_repo_file_paths;
 pub mod post_revert_ingredient;
+
+pub mod post_copy_ingredient;
--- src/endpoints/burrito2/post_copy_ingredient.rs
+++ src/endpoints/burrito2/post_copy_ingredient.rs
@@ -4,55 +4,55 @@ use crate::utils::paths::{check_path_components, check_path_string_components, o
 use crate::utils::response::{
     not_ok_bad_repo_json_response, not_ok_json_response, ok_ok_json_response,
 };
-use copy_dir::copy_dir;
 use rocket::http::{ContentType, Status};
 use rocket::response::status;
 use rocket::{post, State};
 use std::path::{Components, PathBuf};
 use crate::utils::burrito::destination_parent;
 
-/// *`POST /copy/<repo_path>?target_path=<target_path>&delete_src`*
+/// *`POST /ingredient/copy/<repo_path>?src_path=<src_path>&target_path=<target_path>&delete_src`*
 ///
-/// Typically mounted as **`/git/copy/<repo_path>?target_path=<target_path>&delete_src`**
+/// Typically mounted as **`/burrito/copy/<repo_path>?src_path=<src_path>&target_path=<target_path>&delete_src`**
 ///
-/// Copies a repo to a new location, optionally deleting the source.
-#[post("/copy/<repo_path..>?<target_path>&<delete_src>")]
-pub async fn copy_repo(
+/// Copies an ingredient to a new location, optionally deleting the source.
+#[post("/ingredient/copy/<repo_path..>?<src_path>&<target_path>&<delete_src>")]
+pub async fn copy_ingredient(
     state: &State<AppSettings>,
     repo_path: PathBuf,
+    src_path: String,
     target_path: String,
     delete_src: Option<bool>,
 ) -> status::Custom<(ContentType, String)> {
     let path_components: Components<'_> = repo_path.components();
     if check_path_components(&mut path_components.clone())
+        && check_path_string_components(src_path.clone())
         && check_path_string_components(target_path.clone())
     {
         let full_src_path = format!(
-            "{}{}{}",
+            "{}{}{}{}ingredients{}{}",
             &state.repo_dir.lock().unwrap(),
             os_slash_str(),
-            &repo_path.display().to_string()
+            &repo_path.display().to_string(),
+            os_slash_str(),
+            os_slash_str(),
+            &src_path
         );
-        // Src repo must exist
-        if !std::path::Path::new(&full_src_path).is_dir() {
+        // Src ingredient must exist
+        if !std::path::Path::new(&full_src_path).is_file() {
             return not_ok_json_response(
                 Status::BadRequest,
-                make_bad_json_data_response("Source repo not found or not a directory".to_string()),
+                make_bad_json_data_response("Source ingredient not found or not a file".to_string()),
             );
         }
         let full_target_path = format!(
-            "{}{}{}",
+            "{}{}{}{}ingredients{}{}",
             &state.repo_dir.lock().unwrap(),
+            os_slash_str(),
+            &repo_path.display().to_string(),
+            os_slash_str(),
             os_slash_str(),
             &target_path
         );
-        // Target repo dir must not exist
-        if !std::path::Path::new(&full_target_path).is_dir() {
-            return not_ok_json_response(
-                Status::BadRequest,
-                make_bad_json_data_response("Target repo already exists".to_string()),
-            );
-        }
         // Make subdirs if necessary
         let target_parent = destination_parent(full_target_path.clone());
         if !std::path::Path::new(&target_parent).exists() {
@@ -69,24 +69,37 @@ pub async fn copy_repo(
                 }
             }
         }
+        // Maybe make backup file
+        let destination_backup_path = format!("{}.bak", &full_target_path);
+        if std::path::Path::new(&full_target_path).exists() {
+            match std::fs::rename(&full_target_path, &destination_backup_path) {
+                Ok(_) => (),
+                Err(e) => {
+                    return not_ok_json_response(
+                        Status::InternalServerError,
+                        make_bad_json_data_response(format!("Could not write backup file: {}", e)),
+                    )
+                }
+            }
+        }
         // copy repo
-        match copy_dir(full_src_path.clone(), full_target_path) {
+        match std::fs::copy(full_src_path.clone(), full_target_path) {
             Ok(_) => {}
             Err(e) => {
                 return not_ok_json_response(
                     Status::BadRequest,
-                    make_bad_json_data_response(format!("could not copy repo: {}", e).to_string()),
+                    make_bad_json_data_response(format!("could not copy ingredient: {}", e).to_string()),
                 )
             }
         }
-        // Maybe delete src repo
+        // Maybe delete src ingredient
         match delete_src {
             Some(true) => {
-                match std::fs::remove_dir_all(full_src_path) {
+                match std::fs::remove_file(full_src_path) {
                     Ok(_) => { },
                     Err(e) => return not_ok_json_response(
                         Status::BadRequest,
-                        make_bad_json_data_response(format!("could not delete src repo: {}", e).to_string()),
+                        make_bad_json_data_response(format!("could not delete src ingredient: {}", e).to_string()),
                     ),
                 }
             },
