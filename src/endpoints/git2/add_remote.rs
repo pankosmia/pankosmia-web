3e44c22eb1ff2e59d31af507ae1055f939d6b66f
Mark Howe <mvahowe@gmail.com>
2025-08-13 16:58:07 +0200

    More fixes to remote regexes

--- src/endpoints/git2/add_remote.rs
+++ src/endpoints/git2/add_remote.rs
@@ -31,14 +31,14 @@ pub async fn add_remote_to_repo(
             os_slash_str(),
             &repo_path.display().to_string()
         );
-        let remote_name_re = Regex::new(r"^[A-Za-z0-9_-]").unwrap();
+        let remote_name_re = Regex::new(r"[^A-Za-z0-9_-]").unwrap();
         if remote_name_re.is_match(&remote_name) {
             return not_ok_json_response(
                 Status::BadRequest,
                 make_bad_json_data_response("Remote name contains invalid characters".to_string()),
             )
         }
-        let remote_url_re = Regex::new(r"^[A-Za-z0-9_:/@-]").unwrap();
+        let remote_url_re = Regex::new(r"^[A-Za-z0-9_-]+@[A-Za-z0-9_-]+:[A-Za-z0-9_-]+$").unwrap();
         if remote_url_re.is_match(&remote_name) {
             return not_ok_json_response(
                 Status::BadRequest,
