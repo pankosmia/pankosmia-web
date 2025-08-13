81bd33d7cc5ad9a988829a4e503e10aba46c0a08
tidic <tidic84@gmail.com>
2025-08-13 17:43:54 +0200

    first attempt at obs video

--- src/endpoints/audio/obs_para.rs
+++ src/endpoints/audio/obs_para.rs
@@ -1,12 +1,16 @@
 use std::path::{Components, PathBuf};
 use crate::structs::{AppSettings};
-use crate::utils::response::{not_ok_bad_repo_json_response, ok_ok_json_response};
-use rocket::http::{ContentType};
+use crate::utils::response::{not_ok_json_response, not_ok_bad_repo_json_response, ok_ok_json_response};
+use crate::utils::json_responses::{make_bad_json_data_response};
+use rocket::http::{ContentType, Status};
 use rocket::response::status;
 use rocket::serde::json::Json;
 use rocket::{post, State};
 use serde::Deserialize;
 use crate::utils::paths::{check_path_components, os_slash_str};
+use ffmpeg_sidecar::version::ffmpeg_version;
+use ffmpeg_sidecar::command::FfmpegCommand;
+use std::path::Path;
 
 /// *`POST /ffmpeg/video/obs-para/<repo_path>`*
 ///
@@ -27,14 +31,94 @@ pub fn obs_para_video(
 ) -> status::Custom<(ContentType, String)> {
     let path_components: Components<'_> = repo_path.components();
     if check_path_components(&mut path_components.clone()) {
+        let repo_dir = state.repo_dir.lock().unwrap().clone();
         let repo_path = format!(
             "{}{}{}",
-            state.repo_dir.lock().unwrap().clone(),
+            &repo_dir,
             os_slash_str(),
             &repo_path.display().to_string()
         );
-        println!("Repo {} Story {}, Para {}, audio path {}", repo_path, json_form.story_n, json_form.para_n, json_form.audio_path);
-        ok_ok_json_response()
+
+        // Verifier si le chemin est valide
+        if !Path::new(&repo_path).exists() {
+            return not_ok_json_response(
+                Status::NotFound,
+                make_bad_json_data_response(format!("Repo not found: {}", repo_path))
+            );
+        }
+
+        // Formater story et para pour que les chiffres inferieurs a 10 aient un 0 devant
+        let story_string = if json_form.story_n < 10 {
+            format!("0{}", json_form.story_n)
+        } else {
+            json_form.story_n.to_string()
+        };
+        let para_string = if json_form.para_n < 10 {
+            format!("0{}", json_form.para_n)
+        } else {
+            json_form.para_n.to_string()
+        };
+
+        // Chemin du fichier audio
+        let audio_path = format!("{}/ingredients/{}", repo_path, json_form.audio_path);
+        // Vérifier si le fichier audio existe
+        if !Path::new(&audio_path).exists() {
+            return not_ok_json_response(
+                Status::NotFound,
+                make_bad_json_data_response(format!("Audio file not found: {}", audio_path))
+            );
+        }
+        
+        let images_path = format!("{}/git.door43.org/uW/obs_images_360/ingredients/360px/obs-en-{}-{}.jpg", repo_dir, story_string, para_string);
+        // Vérifier si le fichier image existe
+        if !Path::new(&images_path).exists() {
+            println!("Image file not found: {}", images_path);
+            return not_ok_json_response(
+                Status::NotFound,
+                make_bad_json_data_response(format!("Image file not found: {}", images_path))
+            );
+        }
+
+        match ffmpeg_version() {
+            Ok(_) => {
+                
+                let video_content_path = format!("{}/ingredients/video_content", repo_path);
+                if !Path::new(&video_content_path).exists() {
+                    std::fs::create_dir_all(&video_content_path).unwrap();
+                }
+                let video_path = format!("{}/obs-en-{}-{}.mp4", video_content_path, story_string, para_string);
+
+                println!("Video path: {}", video_path);
+                // Créer la vidéo et attendre la fin du traitement
+                let iter = FfmpegCommand::new()
+                    .input(audio_path)
+                    .input(images_path)
+                    .output(video_path.clone())
+                    .spawn().unwrap()
+                    .iter().unwrap();
+
+                for _ in iter {
+                    // On consomme les événements/logs jusqu'à la fin
+                }
+
+                // Vérifier que le fichier a bien été créé
+                if Path::new(&video_path).exists() {
+                    ok_ok_json_response()
+                } else {
+                    not_ok_json_response(
+                        Status::InternalServerError,
+                        make_bad_json_data_response(format!("Échec de création de la vidéo: {}", video_path))
+                    )
+                }
+            },
+            Err(_) => {
+    
+                not_ok_json_response(
+                    Status::InternalServerError,
+                    make_bad_json_data_response("ffmpeg_sidecar not found".to_string())
+                )
+            }
+        }
     } else {
         not_ok_bad_repo_json_response()
     }
