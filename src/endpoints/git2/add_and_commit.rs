0c1f00bb4a967f692ef1ef85305465792855c82c
Mark Howe <mvahowe@gmail.com>
2025-08-19 15:40:10 +0200

    Reworked add and commit endpoint

--- src/endpoints/git2/add_and_commit.rs
+++ src/endpoints/git2/add_and_commit.rs
@@ -1,53 +1,74 @@
 use crate::structs::AppSettings;
-use crate::utils::json_responses::{make_bad_json_data_response};
-use crate::utils::paths::os_slash_str;
-use crate::utils::response::{not_ok_json_response, ok_ok_json_response};
+use crate::utils::json_responses::make_bad_json_data_response;
+use crate::utils::paths::{check_path_components, os_slash_str};
+use crate::utils::response::{
+    not_ok_bad_repo_json_response, not_ok_json_response, ok_ok_json_response,
+};
 use git2::Repository;
 use rocket::http::{ContentType, Status};
 use rocket::response::status;
-use rocket::{get, State};
-use std::path::PathBuf;
+use rocket::{post, State};
+use std::path::{Components, PathBuf};
+use rocket::serde::Deserialize;
+use rocket::serde::json::Json;
 
-/// *`GET /add-and-commit/<repo_path>`*
+/// *`POST /add-and-commit/<repo_path>`*
 ///
 /// Typically mounted as **`/git/add-and-commit/<repo_path>`**
 ///
 /// Adds and commits modified files for a given repo.
-#[get("/add-and-commit/<repo_path..>")]
+
+#[derive(Deserialize)]
+pub struct AddCommitForm {
+    commit_message: String,
+}
+
+#[post("/add-and-commit/<repo_path..>", format = "json", data = "<json_form>")]
 pub async fn add_and_commit(
     state: &State<AppSettings>,
     repo_path: PathBuf,
+    json_form: Json<AddCommitForm>,
 ) -> status::Custom<(ContentType, String)> {
-    let repo_path_string: String = state.repo_dir.lock().unwrap().clone()
-        + os_slash_str()
-        + &repo_path.display().to_string().clone();
-    let result = match Repository::open(repo_path_string) {
-        Ok(repo) => {
-            repo.index()
-                .unwrap()
-                .add_all(&["."], git2::IndexAddOption::DEFAULT, None)
+    let path_components: Components<'_> = repo_path.components();
+    if check_path_components(&mut path_components.clone()) {
+        let repo_path_string = format!(
+            "{}{}{}",
+            state.repo_dir.lock().unwrap().clone(),
+            os_slash_str(),
+            &repo_path.display().to_string().clone()
+        );
+        let result = match Repository::open(repo_path_string) {
+            Ok(repo) => {
+                repo.index()
+                    .unwrap()
+                    .add_all(&["."], git2::IndexAddOption::DEFAULT, None)
+                    .unwrap();
+                repo.index().unwrap().write().unwrap();
+                let mut index = repo.index().unwrap();
+                let oid = index.write_tree().unwrap();
+                let signature = repo.signature().unwrap();
+                let parent_commit = repo.head().unwrap().peel_to_commit().unwrap();
+                let tree = repo.find_tree(oid).unwrap();
+                repo.commit(
+                    Some("HEAD"),
+                    &signature,
+                    &signature,
+                    json_form.commit_message.as_str(),
+                    &tree,
+                    &[&parent_commit],
+                )
                 .unwrap();
-            repo.index().unwrap().write().unwrap();
-            let mut index = repo.index().unwrap();
-            let oid = index.write_tree().unwrap();
-            let signature = repo.signature().unwrap();
-            let parent_commit = repo.head().unwrap().peel_to_commit().unwrap();
-            let tree = repo.find_tree(oid).unwrap();
-            repo.commit(
-                Some("HEAD"),
-                &signature,
-                &signature,
-                "Updated by Pithekos",
-                &tree,
-                &[&parent_commit],
-            )
-            .unwrap();
-            ok_ok_json_response()
-        }
-        Err(e) => not_ok_json_response(
-            Status::InternalServerError,
-            make_bad_json_data_response(format!("could not add/commit repo: {}", e).to_string()),
-        ),
-    };
-    result
+                ok_ok_json_response()
+            }
+            Err(e) => not_ok_json_response(
+                Status::InternalServerError,
+                make_bad_json_data_response(
+                    format!("could not add/commit repo: {}", e).to_string(),
+                ),
+            ),
+        };
+        result
+    } else {
+        not_ok_bad_repo_json_response()
+    }
 }
