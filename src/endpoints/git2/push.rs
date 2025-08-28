df06e6df164f4414422ae8e34b3e38a66fadceeb
Mark Howe <mvahowe@gmail.com>
2025-08-28 16:55:40 +0200

    Push using https

--- src/endpoints/git2/push.rs
+++ src/endpoints/git2/push.rs
@@ -16,10 +16,11 @@ use std::path::{Components, PathBuf};
 use std::sync::atomic::Ordering;
 use crate::utils::paths::home_dir_string;
 
-#[derive(Deserialize)]
+#[derive(Deserialize, Clone)]
 pub struct PushForm {
     remote: String,
     cred_type: String,
+    username: Option<String>,
     pass_key: Option<String>,
 }
 /// *`POST /push/<remote>/<repo_path>`*
@@ -27,18 +28,20 @@ pub struct PushForm {
 /// Typically mounted as **`/push/<remote>/<repo_path>`**
 ///
 /// Push to remote from the given repo path. In the JSON body,
-/// - cred_type is the type of SSH key, eg 'rsa'
-/// - pass_key is the optional passkey for the SSH key
+/// - cred_type is the type of SSH key, eg 'rsa', or 'https'
+/// - pass_key is the optional passkey for the SSH key or the required password for plaintext
+/// - username is required for HTTPS only
 #[post("/push/<repo_path..>", format = "json", data = "<json_form>")]
 pub async fn push_repo(
     state: &State<AppSettings>,
     repo_path: PathBuf,
     json_form: Json<PushForm>,
 ) -> status::Custom<(ContentType, String)> {
+    
     if !NET_IS_ENABLED.load(Ordering::Relaxed) {
         return not_ok_offline_json_response();
     }
-    let git_credentials_callback = |_user: &str,
+    let git_ssh_credentials_callback = |_user: &str,
                                     _user_from_url: Option<&str>,
                                     _cred: git2::CredentialType|
      -> Result<Cred, git2::Error> {
@@ -76,7 +79,12 @@ pub async fn push_repo(
         };
         cred
     };
-
+    
+    let git_https_credentials_callback = |_: &str, _: Option<&str>, _: git2::CredentialType| Cred::userpass_plaintext(
+        &json_form.username.as_ref().expect("No username").as_str(),
+        &json_form.pass_key.as_ref().expect("No password key").as_str(),
+    );
+    
     let path_components: Components<'_> = repo_path.components();
     if check_path_components(&mut path_components.clone()) {
         let repo_path_string = format!(
@@ -102,8 +110,13 @@ pub async fn push_repo(
                 let head = repo.head().expect("Could not locate head");
                 let head_branch_name = head.name().expect("Could not get branch name from head");
                 let mut remote_callbacks = RemoteCallbacks::new();
-                remote_callbacks.credentials(git_credentials_callback);
+                remote_callbacks.credentials(git_ssh_credentials_callback);
                 let mut push_options = git2::PushOptions::new();
+                if json_form.cred_type == "https" {
+                    remote_callbacks.credentials(git_https_credentials_callback);
+                } else {
+                    remote_callbacks.credentials(git_ssh_credentials_callback);
+                }
                 push_options.remote_callbacks(remote_callbacks);
                 match remote_object.push::<&str>(&[head_branch_name], Some(&mut push_options)) {
                     Ok(_) => ok_ok_json_response(),
