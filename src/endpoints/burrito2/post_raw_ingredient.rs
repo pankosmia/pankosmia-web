8f70af15de4a820f08f83e7c74796d4a309c7138
Mark Howe <mvahowe@gmail.com>
2025-11-25 18:01:29 +0100

    Add optional update_ingredients param to post_raw_ingredient

--- src/endpoints/burrito2/post_raw_ingredient.rs
+++ src/endpoints/burrito2/post_raw_ingredient.rs
@@ -1,4 +1,4 @@
-use crate::structs::AppSettings;
+use crate::structs::{AppSettings, BurritoMetadata};
 use crate::utils::json_responses::{make_bad_json_data_response};
 use crate::utils::paths::{check_path_components, check_path_string_components, os_slash_str};
 use crate::utils::response::{not_ok_json_response, ok_ok_json_response, not_ok_bad_repo_json_response};
@@ -8,9 +8,9 @@ use rocket::serde::json::Json;
 use rocket::{post, State};
 use serde_json::Value;
 use std::path::{Components, PathBuf};
-use crate::utils::burrito::destination_parent;
+use crate::utils::burrito::{destination_parent, ingredients_metadata_from_files, ingredients_scopes_from_files};
 
-/// *`POST /ingredient/raw/<repo_path>?ipath=my_burrito_path`*
+/// *`POST /ingredient/raw/<repo_path>?ipath=my_burrito_path&update_ingredients`*
 ///
 /// Typically mounted as **`/burrito/ingredient/raw/<repo_path>?ipath=my_burrito_path`**
 ///
@@ -18,14 +18,16 @@ use crate::utils::burrito::destination_parent;
 ///
 /// /// The target file must exist, ie this is not the way to add new ingredients to a Burrito
 #[post(
-    "/ingredient/raw/<repo_path..>?<ipath>",
+    "/ingredient/raw/<repo_path..>?<ipath>&<update_ingredients>",
     format = "json",
     data = "<json_form>"
 )]
+#[allow(irrefutable_let_patterns)]
 pub async fn post_raw_ingredient(
     state: &State<AppSettings>,
     repo_path: PathBuf,
     ipath: String,
+    update_ingredients: Option<String>,
     json_form: Json<Value>,
 ) -> status::Custom<(ContentType, String)> {
     let path_components: Components<'_> = repo_path.components();
@@ -72,6 +74,69 @@ pub async fn post_raw_ingredient(
                 make_bad_json_data_response(format!("Could not write to {}: {}", ipath, e)),
             ),
         };
+        if update_ingredients.is_some() {
+            // Get metadata as struct
+            let app_resources_dir = format!("{}", &state.app_resources_dir);
+            let path_to_repo_metadata = format!(
+                "{}{}metadata.json",
+                &full_repo_path,
+                os_slash_str(),
+            );
+            let metadata_string = match std::fs::read_to_string(&path_to_repo_metadata) {
+                Ok(v) => v,
+                Err(e) => {
+                    return not_ok_json_response(
+                        Status::InternalServerError,
+                        make_bad_json_data_response(format!(
+                            "Could not load metadata as string: {}",
+                            e
+                        )),
+                    )
+                }
+            };
+            // Make struct from metadata
+            let mut metadata_struct: BurritoMetadata = match serde_json::from_str(&metadata_string) {
+                Ok(v) => v,
+                Err(e) => {
+                    return not_ok_json_response(
+                        Status::InternalServerError,
+                        make_bad_json_data_response(format!("Could not parse metadata: {}", e)),
+                    );
+                }
+            };
+            // Add ingredient record and currentScope value for USFM
+            if let mut ingredients = metadata_struct.ingredients.lock().unwrap() {
+                let new_ingredients = ingredients_metadata_from_files(app_resources_dir.clone(), full_repo_path.clone());
+                *ingredients = new_ingredients;
+            }
+            if let type_info = metadata_struct.r#type {
+                let mut type_ob = type_info.as_object().unwrap().clone();
+                let flavor_type_ob = type_ob["flavorType"].as_object_mut().unwrap();
+                let new_current_scope = ingredients_scopes_from_files(app_resources_dir, full_repo_path.clone());
+                flavor_type_ob["currentScope"] = serde_json::from_str(serde_json::to_string(&new_current_scope).unwrap().as_str()).unwrap();
+                metadata_struct.r#type = serde_json::from_str(serde_json::to_string(&type_ob).unwrap().as_str()).unwrap();
+            }
+
+            // Write metadata
+            let metadata_output_string = match serde_json::to_string(&metadata_struct) {
+                Ok(s) => s,
+                Err(e) => {
+                    return not_ok_json_response(
+                        Status::InternalServerError,
+                        make_bad_json_data_response(format!("Could not make metadata as JSON: {}", e)),
+                    )
+                }
+            };
+            match std::fs::write(path_to_repo_metadata, &metadata_output_string) {
+                Ok(_) => (),
+                Err(e) => {
+                    return not_ok_json_response(
+                        Status::InternalServerError,
+                        make_bad_json_data_response(format!("Could not write metadata to repo: {}", e)),
+                    )
+                }
+            }
+        }
         ok_ok_json_response()
     } else {
         not_ok_bad_repo_json_response()
