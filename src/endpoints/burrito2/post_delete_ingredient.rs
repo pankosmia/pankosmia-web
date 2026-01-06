c3976b0b165cce2cf868a374b8f18c79add6deee
Mark Howe <mvahowe@gmail.com>
2026-01-06 13:36:24 +0100

    Lock for delete ingredient

--- src/endpoints/burrito2/post_delete_ingredient.rs
+++ src/endpoints/burrito2/post_delete_ingredient.rs
@@ -21,8 +21,9 @@ pub async fn post_delete_ingredient(
     ipath: String,
 ) -> status::Custom<(ContentType, String)> {
     let path_components: Components<'_> = repo_path.components();
+    let repo_dir = state.repo_dir.lock().expect("lock for repo dir");
     let full_repo_path =
-        state.repo_dir.lock().unwrap().clone() + os_slash_str() + &repo_path.display().to_string();
+        repo_dir.clone() + os_slash_str() + &repo_path.display().to_string();
     if check_path_components(&mut path_components.clone())
         && check_path_string_components(ipath.clone())
         && std::fs::metadata(&full_repo_path).is_ok()
